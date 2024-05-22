3d6906a7438b9a9afae8ed43b8235db9b2eabc82
Yoosup Song <Youssupsong@gmail.com>
2024-05-22 14:46:58 -0400

    Basic Vue setup with DaisyUI/TailwindCSS

--- frontend/client/README.md
+++ frontend/client/README.md
@@ -1,4 +1,4 @@
-# YousupVillage
+# client
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ frontend/client/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <Navbar />
+    </nav>
+  </header>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
